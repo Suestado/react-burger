a3fc528248142f0c550a9ef1c8152407ec891020
Sergey Kiselev <sk.mails@yandex.ru>
2024-01-25 13:20:20 +0300

    Fix: Scroll bar fixed

--- src/components/IngredientsSwitchbar/IngredientsSwitchbar.tsx
+++ src/components/IngredientsSwitchbar/IngredientsSwitchbar.tsx
@@ -8,19 +8,30 @@ interface IIngredientsSwitchbar {
 
 const IngredientsSwitchbar: FC<IIngredientsSwitchbar> = ({activeTab}): React.ReactElement => {
 
+  const scrollToTargetBlock = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const clickTargetElement = event.target as HTMLElement;
+    const idToScroll = clickTargetElement.closest('a')?.getAttribute('href')?.slice(1)
+    const targetElementToScroll = document.getElementById(String(idToScroll));
+
+    targetElementToScroll?.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+
   return (
-    <div className={styles.ingredientsSwitchbar}>
-      <a className={styles.link} href="#bun">
+    <div className={styles.ingredientsSwitchbar} >
+      <a className={styles.link} href="#bun" onClick={scrollToTargetBlock}>
         <Tab value="one" active={activeTab === 'bun'} onClick={() => void 0}>
           Булки
         </Tab>
       </a>
-      <a className={styles.link} href="#sauce">
+      <a className={styles.link} href="#sauce" onClick={scrollToTargetBlock}>
         <Tab value="two" active={activeTab === 'sauce'} onClick={() => void 0}>
           Соусы
         </Tab>
       </a>
-      <a className={styles.link} href="#main">
+      <a className={styles.link} href="#main" onClick={scrollToTargetBlock}>
         <Tab value="three" active={activeTab === 'main'} onClick={() => void 0}>
           Начинки
         </Tab>
